975fbd4694b1220192292d025dfe535e3a30e0f6
Egor <ekurito@gmail.com>
2019-11-20 18:39:46 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+var apiKey *string
+
+type Source struct {
+	ID   interface{} `json:"id"`
+	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+type Search struct {
+	SearchKey  string
+	NextPage   int
+	TotalPages int
+	Results    Results
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Interal server error"))
+		return
+	}
+
+	params := u.Query()
+	searchKey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchKey)
+	fmt.Println("Results page is: ", page)
+}
+
+func main() {
+	apiKey = flag.String("apiKey", "", "Newsapi.org acces key")
+	flag.Parse()
+
+	if *apiKey == "" {
+		log.Fatal("apiKey must be set")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	mux := http.NewServeMux()
+
+	// Work with static files
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/search", searchHandler)
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
